54306e67b740646cb204a5db3319c7f39957fc29
Cooper Stansbury <cstansbu@umich.edu>
2020-04-10 12:07:13 -0400

    update CLAMP parser

--- parse_clamp/README.md
+++ parse_clamp/README.md
@@ -1,2 +1,15 @@
-# XMI Parser
-TODO
+# CLAMP Annotation Parser
+This is a simple tool to convert a directory of `.xmi` files containing annotations to a one-hot encoded `.csv` file for analysis. The tool assumes all annotations are in separate files in the directory `parse_clamp/input_data/`, though a different target may be specified.
+
+## Dependencies
+I have not written this to be production level code. It has been tested on OSX systems only. It is written for Python3.7.6 with no regard for backwards compatibility. My conda env has been exported in the file `annotation_tools/parse_clamp/FULL_CONDA_ENV.txt`.
+
+
+## Usage
+The tool's use is straightforward:
+
+```
+python3 parse_clamp/ [--i <OPTIONAL TARGET DIR>]
+```
+
+By default the tool will parse documents in the path `annotation_tools/parse_clamp/input_data/`.
--- parse_clamp/__main__.py
+++ parse_clamp/__main__.py
@@ -6,17 +6,16 @@ import re
 import os
 import spacy
 from datetime import datetime
+from sklearn.preprocessing import MultiLabelBinarizer
 
 # local imports
 import parse_xmi_files as pxmi
-import rater_dataframe_ops as rdfo
 
 ### NOTE: this silences numpy divide errors
 np.seterr(divide='ignore', invalid='ignore')
 
-
 def get_date():
-    """A funtion to return today's date as a stringself.
+    """A funtion to return today's date as a string.
 
     Args:
         - NA
@@ -28,96 +27,93 @@ def get_date():
     return datetime.today().strftime("%m-%d-%Y")
 
 
-def get_rater_name(dir_name):
-    """A function to retreive the 'rater name' from an
-    input directory label.
+def mark_empty_annotations(df):
+    """A function to mark sentences that were not annotated
+    using apply design pattern, replace with string 'blank'
 
     Args:
-        - dir_name (string): the string representing the input path
+        - df (pd.DataFrame): a data frame with annotations (including blanks)
 
     Returns:
-        - rater_name (string) a label to use for the 'rater'
-
-     use input dirname as rater name """
-    return str(os.path.dirname(dir_name)).split("/")[1]
-
-
-def get_annotation_values(nested_annotation_column1, nested_annotation_column2):
-    """A function to retrieve the annotation values (unique). This is
-    Used to build a one-hot map.
-
-    Args:
-        - nested_annotation_column1 (pd.Series)
-        - nested_annotation_column2 (pd.Series)
-
-    Returns:
-        - annotation labels (list of string)
-    """
-    flat_list1 = [item for sublist in nested_annotation_column1 for item in sublist]
-    flat_list2 = [item for sublist in nested_annotation_column2 for item in sublist]
-    return  list(set(flat_list1 + flat_list2))
-
-def write_agreement_summary(p_df, p_name, q_df, q_name, annotation_values):
-    """A function to compile and write the annotations to a cvs file.
-
-    Args:
-        - p_df (pd.DataFrame): a dataframe of annotation for rater 1
-        - p_name (string): the label representing annotator 1
-        - q_df (pd.DataFrame): a dataframe of annotation for rater 2
-        - q_name (string): the label representing annotator 2
-        - annotation_values (list of string): a list of unique annotation values
-
-    Returns:
-        NA. writes a csv file out.
+        - df (pd.DataFrame): modified in place
     """
 
-    # get annotation map object
-    print('Making annotation map object...')
-    annotation_map_df = p_df[['file_name']].copy()
+    def apply_check_annotation_list(row):
+        """ apply function to test if len(annotations) = 0 """
+        if len(row) == 0:
+            return(['blank'])
+        else:
+            return(row)
 
-    # sort the annotation list to make sure order
-    # doesn't affect the Kappa score
-    annotation_map_df[p_name] = p_df['annotation_list_with_empty'].apply(lambda x: sorted(x)).copy()
-    annotation_map_df[q_name] = q_df['annotation_list_with_empty'].apply(lambda x: sorted(x)).copy()
+    df['annotation_list_with_empty'] = df['annotation_list'].apply(lambda row:apply_check_annotation_list(row))
+    return df
 
-    # add simple flag for easy identification of disagreements
-    annotation_map_df['Agree?'] = np.where(annotation_map_df[p_name] == annotation_map_df[q_name] ,"Yes", "No")
-    annotation_map_df['Sentence'] = p_df[['raw_text']].copy()
 
-    # write csv object
-    save_path = 'annotation_summary' + get_date() + '.csv'
+def normalize_annotations(df):
+    """A function to convert list of annotations into new columns (one-hot).
 
-    annotation_map_df.to_csv(save_path)
+    NOTE: this does not account for labels that were defined in the TypeDef,
+    but were not used by the annotator.
 
-    print('DONE. Wrote output to: ', save_path)
+    Args:
+        - df (pd.DataFrame): a data frame with annotations (including blanks)
+
+    Returns:
+        - df (pd.DataFrame): modified, returns new (may be a shallow copy)
+     """
+    nested_list = df['annotation_list_with_empty']
+    mlb = MultiLabelBinarizer()
+    ont_hot_df = pd.DataFrame(mlb.fit_transform(df.pop('annotation_list_with_empty')),
+                          columns=mlb.classes_,
+                          index=df.index)
+    new_df = pd.concat([df, ont_hot_df], axis=1)
+    new_df['annotation_list_with_empty'] = nested_list
+    return new_df
+
+
+def get_rater_dataframe(annotations):
+    """A function to build the rater data structure from a
+    list of parsed annotations.
+
+    Args:
+        - annotations (list): a list of annotations
+
+    Returns:
+        - df (pd.DataFrame): a data frame of annotations
+    """
+    list_df = []
+    for parsed_files in annotations:
+        file_name = parsed_files[0]
+        annotations = parsed_files[1]
+        tmp_df = pd.DataFrame(annotations, columns=['raw_text','annotation_list'])
+        tmp_df['file_name'] = file_name
+        list_df.append(tmp_df)
+    df = pd.concat(list_df)
+
+    # handle blank annotations and make one-hot encoded
+    # columns
+    df = mark_empty_annotations(df)
+    df = normalize_annotations(df)
+    return df
 
 
 if __name__ == '__main__':
-    program_description = "A command-line tool to calculate kappa values based \
-    on CLAMP annotations."
-    parser = argparse.ArgumentParser(description=program_description)
-    parser.add_argument("--p", help="directory of annotations for annotator 1")
-    parser.add_argument("--q", help="directory of annotations for annotator 2")
+    desc = """ A Python3 commandline tool to parse CLAMP files (.xmi)
+    into .csv format. """
 
+    # handle input arguments
+    # defaults to 'input_data/' dir, but can handle a different target
+    default_path = "parse_clamp/input_data/"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("--i",  nargs='?', default=default_path,
+                        help="directory of annotations")
     args = parser.parse_args()
 
-    p_name = get_rater_name(args.p)
-    q_name = get_rater_name(args.q)
+    # parse CLAMP files into list and normalize
+    # into a dataframe
+    annotation_list = pxmi.parse_xmi_dir(args.i)
+    df = get_rater_dataframe(annotation_list)
 
-    # parse CLAMP files (xmi)
-    p_list = pxmi.parse_xmi_dir(args.p)
-    q_list = pxmi.parse_xmi_dir(args.q)
-
-    # convert annotations to dataframes for easier use
-    p_df = rdfo.get_rater_dataframe(p_list, p_name)
-    q_df = rdfo.get_rater_dataframe(q_list, q_name)
-
-    # detect annotation values
-    annotation_values = get_annotation_values(p_df['annotation_list_with_empty'],
-                                              q_df['annotation_list_with_empty'])
-    p_df = rdfo.correct_for_missing_labels(p_df, annotation_values)
-    q_df = rdfo.correct_for_missing_labels(q_df, annotation_values)
-
-
-    # write report
-    write_agreement_summary(p_df, p_name, q_df, q_name, annotation_values)
+    # write the csv out to a new file (with today's date)
+    save_path = 'annotations' + get_date() + '.csv'
+    df.to_csv(save_path)
